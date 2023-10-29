ad27a786328d6875f52183039e4692b70e368b02
Steven <139715581+StevenSYS@users.noreply.github.com>
2023-10-29 18:45:42 +0000

    Fix bug

--- von.js
+++ von.js
@@ -1,7 +1,7 @@
 function PVON() {
 	var prn1 = document.getElementById("first").value;
 	var prn2 = document.getElementById("second").value;
-	if (prn1.length == 0 || prn2.length == 0) {
+	if (prn1.trim().length == 0 || prn2.trim().length == 0) {
 		document.getElementById("pvon").innerHTML = "";
 	} else {
 		if (prn1.toLowerCase() == "he" && prn2.toLowerCase() == "him" || prn1.toLowerCase() == "she" && prn2.toLowerCase() == "her") {
@@ -18,7 +18,7 @@ function PVON() {
 function GVON() {
 	var gnd = document.getElementById("gender").value;
 	console.log(gnd.toLowerCase());
-	if (gnd.length == 0) {
+	if (gnd.trim().length == 0) {
 		document.getElementById("gvon").innerHTML = "";
 	} else {
 		if (gnd.toLowerCase() == "male" || gnd.toLowerCase() == "female") {
