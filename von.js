160cff57ecf4876714b1c9f9eaa471e9ad37d4a4
Steven <139715581+StevenSYS@users.noreply.github.com>
2023-11-06 01:59:34 +0000

    Remove a joke
    I had to remove the "Ge/Gim" joke because I found out that this is a "real" pronoun >:(

--- von.js
+++ von.js
@@ -5,13 +5,9 @@ function PVON() {
 		document.getElementById("pvon").innerHTML = "";
 	} else {
 		if (prn1.trim().toLowerCase() == "he" && prn2.trim().toLowerCase() == "him" || prn1.trim().toLowerCase() == "she" && prn2.trim().toLowerCase() == "her") {
-				document.getElementById("pvon").innerHTML = prn1 + "/" + prn2 + " is valid!";
+			document.getElementById("pvon").innerHTML = prn1 + "/" + prn2 + " is valid!";
 		} else {
-			if (prn1.trim().toLowerCase() == "ge" && prn2.trim().toLowerCase() == "gim") {
-				document.getElementById("pvon").innerHTML = prn1 + "/" + prn2 + " is valid ;)";
-			} else {
-				document.getElementById("pvon").innerHTML = prn1 + "/" + prn2 + " is NOT valid!!!";
-			}
+			document.getElementById("pvon").innerHTML = prn1 + "/" + prn2 + " is NOT valid!!!";
 		}
 	}
 }
@@ -23,11 +19,7 @@ function GVON() {
 		if (gnd.trim().toLowerCase() == "male" || gnd.trim().toLowerCase() == "female") {
 			document.getElementById("gvon").innerHTML = gnd + " is valid!";
 		} else {
-			if (gnd.trim().toLowerCase() == "gemale") {
-				document.getElementById("gvon").innerHTML = gnd + " is valid ;)";
-			} else {
-				document.getElementById("gvon").innerHTML = gnd + " is NOT valid!!!";
-			}
+			document.getElementById("gvon").innerHTML = gnd + " is NOT valid!!!";
 		}
 	}
 }
